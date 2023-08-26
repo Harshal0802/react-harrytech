ac8f954dc14b05b700ee2dae390b1b0c4851ae24
Harshal <harshrathod1111@gmail.com>
2023-08-26 13:05:03 +0530

    implemented contact and services page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
